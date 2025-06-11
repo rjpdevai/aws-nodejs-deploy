66161ebed169c1704b8a8bdc6a0933ac4c6a5cc1
Rjpdevai <rjpdevai@gmail.com>
2025-06-11 04:05:36 -0400

    testing api

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/api/get', (req, res) => {
     res.send({ message: "Hello world from node js" })
 });
 
+app.get('/api/get_user', (req, res) => {
+    res.send({ user: { name: "John Doe", age: 30 } });
+});
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`App running at port ${PORT}`);
 });
